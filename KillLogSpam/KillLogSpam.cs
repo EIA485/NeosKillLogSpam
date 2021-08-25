fb28573f8dac3e211d37a368ab68461356e6b3ee
eia485 <eia485git@gmail.com>
2021-08-24 18:05:50 -0700

    Add project files.

---
+++ KillLogSpam/KillLogSpam.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+namespace KillLogSpam
+{
+    public class KillLogSpam : NeosMod
+    {
+        public override string Name => "KillLogSpam";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosKillLogSpam/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.KillLogSpam");
+            harmony.PatchAll();
+
+        }
+        [HarmonyPatch(typeof(WorldConfiguration), "FieldChanged")]
+        class KillLogSpamPatch
+        {
+            public static bool Prefix(IChangeable obj)
+            {
+                return false;
+            }
+        }
+    }
+}
